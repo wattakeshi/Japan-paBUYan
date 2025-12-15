e4860fea794f17606b2fb34185f6682fe045f738
wattakeshi <takeshi.watanabe@live.com>
2025-12-16 00:14:04 +0900

    fix Categories Fetch

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 const Categories = () => {
+
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const API_URL = "http://localhost:1337/api/categories?populate=*"
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        setCategories(json.data)
+      }catch (error){
+        console.log("Fetch error",error)
+      }finally{
+        setLoading(false)
+      }};
+
+                      fetchCategories();
+            console.log("terminou o fetch")
+    },[])
+
+
+
+    //
+
+
+    if(loading){
+      return <div>Loading Categories...</div>
+    }
+
   return (
     <div className='  h-[780px] w-full pt-16'>
                  <div className='flex justify-between'>
@@ -9,7 +38,11 @@ const Categories = () => {
          <p className='pb-8 w-3/4 mx-auto text-cor3 text-center text-[16]'>veja nossos produtos, separados por kaeporjasjeopsajeopasjeaospejaoskeoapskeopsasoeihasioeas</p>
                  </div>
        <div>
-          
+          {categories.map((cate)=>(
+            <div key={cate.id}> 
+            <p>{cate.name}</p>
+            </div>)
+          )}
 
        </div>
     </div>

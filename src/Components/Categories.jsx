45bcb7bd13bd33a4b2dd12ed080f8ae0ac14f84e
wattakeshi <takeshi.watanabe@live.com>
2025-12-17 00:07:11 +0900

    feat. mobile responsive design

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -32,15 +32,17 @@ const Categories = () => {
     }
 
   return (
-    <div className='  h-[780px] w-full pt-16'>
+    <div className='  h-[780px] w-full pt-8 md:pt-16'>
                  <div className='flex justify-between'>
          <h1 className='pb-8 w-3/4 mx-auto text-cor3 text-center text-[40px] font-bold'> categoriadeprodutos </h1>
-         <p className='pb-8 w-3/4 mx-auto text-cor3 text-center text-[16]'>veja nossos produtos, separados por kaeporjasjeopsajeopasjeaospejaoskeoapskeopsasoeihasioeas</p>
+         <p className='hidden md:block pb-8 w-3/4 mx-auto text-cor3 text-center text-[16]'>veja nossos produtos, separados por kaeporjasjeopsajeopasjeaospejaoskeoapskeopsasoeihasioeas</p>
                  </div>
-       <div>
-          {categories.map((cate)=>(
-            <div key={cate.id}> 
-            <p>{cate.name}</p>
+       <div className='grid grid-cols-2 '>
+          {categories.map((category)=>(
+            <div key={category.id} className='bg-cor2 items-center md:w-2/3 md:h-[400px] h-[400px] justify-center mb-8 md:mx-auto rounded-4xl hover:bg-cor4 mx-2
+
+            '> 
+            <p className='text-cor5 font-bold text-[28px] text-center pt-8'>{category.name}</p>
             </div>)
           )}
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,11 +5,15 @@ const Header = () => {
   return (
  <>
  <div>
-
+    <div className="hidden md:block">
     <div className=' bg-cor2 h-[740px] w-full py-20  '>
           <h1 className='py-8 w-3/4 mx-auto font-bold text-cor5 text-center text-[45px]'> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
       <p className='py-8 w-4/5 mx-auto text-cor5 text-center text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fringilla nunc in molestie feugiat. Nunc auctor consectetur elit, quis pulvina. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fringilla nunc in molestie feugiat</p>
       <Search />
+    </div>
+    </div>
+    <div className='block md:hidden lg:hidden'>
+    <img src={banner} alt="PaBUYan Banner" />
     </div>
   </div>  
     </>

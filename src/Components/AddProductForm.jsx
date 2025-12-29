daf9476d09626a84a74f78288c60ab7b046bd5b4
wattakeshi <takeshi.watanabe@live.com>
2025-12-29 17:06:40 +0900

    add: WishList setitem in LocalStorage

--- src/Components/AddProductForm.jsx
+++ src/Components/AddProductForm.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AddProductForm = () => {
+
+  const [wishList, setWishList] = useState(()=>{
+    const saved = localStorage.getItem("pabuyan_wishList")
+    return saved? JSON.parse(saved) : []
+  })
+
+  useEffect(() => {
+  localStorage.setItem('pabuyan_wishList', JSON.stringify(wishList));
+}, [wishList]);
+  
+
+  const [productName, setProductName] = useState("");
+  const [productDetail, setProductDetail] = useState("");
+  const [productQty, setProductQty] = useState(1);
+  const [productReference, setProductReference] = useState(null); 
+
+
+  const handleAddProduct = () => {
+    const newProduct = {
+      id: Date.now(),
+      name: productName,
+      details: productDetail,
+      qyt: productQty,
+      reference: productReference
+    }
+    setWishList([...wishList, newProduct])
+    
+    setProductName("")
+    setProductDetail("")
+    setProductQty(1)
+    setProductReference(null)
+  }
   return (
 <div className="flex flex-col items-center w-full max-w-lg mx-auto gap-6 mt-10">
   
 <div className="w-full flex flex-col sm:flex-row items-center gap-4">
      <label className="w-full sm:w-32 text-center sm:text-right font-bold text-cor5/80">Product </label>
      <input 
-      type="text"
+      type="text" value={productName} onChange={(e)=>setProductName(e.target.value)}
       placeholder="Product name"
       className="w-full flex-1 bg-white/10 border border-white/20 rounded-full px-6 py-3 text-white outline-none focus:border-cor5 transition-all"
     />
@@ -16,7 +48,7 @@ const AddProductForm = () => {
 <div className="w-full flex flex-col sm:flex-row items-center gap-4">
      <label className="w-full sm:w-32 text-center sm:text-right font-bold text-cor5/80">Details </label>
      <input 
-      type="text"
+      type="text" value={productDetail} onChange={(e)=>setProductDetail(e.target.value)}
       placeholder="Color / Size / Flavor / Brand"
       className="w-full flex-1 bg-white/10 border border-white/20 rounded-full px-6 py-3 text-white outline-none focus:border-cor5"
     />
@@ -26,7 +58,7 @@ const AddProductForm = () => {
        <p className="sm:w-32 mx-auto text-center sm:text-right font-bold text-cor5/80">Qty</p>
        </div>
         <input 
-        type="number"
+        type="number" value={productQty} onChange={(e)=>setProductQty(e.target.value)}
         className="w-full bg-white/10 border border-white/20 rounded-full px-6 py-3 text-white"
       />
     
@@ -35,13 +67,18 @@ const AddProductForm = () => {
 <div className="w-full flex flex-col sm:flex-row items-center gap-4 mt-2">
     <div className="sm:w-32"></div> 
      <label className="flex-1 cursor-pointer bg-cor5 hover:bg-opacity-80 text-white font-bold py-3 px-6 rounded-full text-center transition-all">
-      <span>Upload image 📸</span>
-      <input type="file" className="hidden" />
+     <span>{productReference ? "Image Selected!" : "Upload image 📸"}</span>
+          <input 
+            type="file" 
+            className="hidden" 
+            onChange={(e) => setProductReference(e.target.files[0])} 
+          />
     </label>
 </div>
 
-  {/* Botão Adicionar à Lista */}
-  <button className="mt-4 w-full bg-white text-cor1 font-black py-4 rounded-full shadow-xl hover:scale-105 transition-transform">
+  <button className="mt-4 w-full bg-white text-cor1 font-black py-4 rounded-full shadow-xl hover:scale-105 transition-transform"
+  onClick={handleAddProduct}
+  >
     ADD TO LIST
   </button>
 
--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -9,19 +9,19 @@ const NavBar = () => {
       <img src={logo} alt="JapanpaBUYan" className="h-16 p-0 rounded-full" />
 
       <div className='flex '>
-        <Link to="/" className='text-cor5 opacity-100 px-8  hover:bg-cor2 !font-bold'>HOME</Link>
-        <Link to="/PabuyanNews" className='text-cor5 px-8 opacity-100  hover:bg-cor2 !font-bold'>PabuyanNews</Link>
-        <Link to="/AboutUS" className='text-cor5 px-8 opacity-100  hover:bg-cor2 !font-bold'>AboutUS</Link>
+        <Link to="/" className='text-cor5 opacity-100 px-8  hover:bg-cor2 font-bold'>HOME</Link>
+        <Link to="/PabuyanNews" className='text-cor5 px-8 opacity-100  hover:bg-cor2 font-bold'>PabuyanNews</Link>
+        <Link to="/AboutUS" className='text-cor5 px-8 opacity-100  hover:bg-cor2 font-bold'>AboutUS</Link>
        
       </div>
 
-       <Link to="/Profile" className='text-cor5 opacity-100 px-8 hover:bg-cor2 !font-bold'>ACCOUNT</Link>
+       <Link to="/Profile" className='text-cor5 opacity-100 px-8 hover:bg-cor2 font-bold'>ACCOUNT</Link>
     </div>
     </div>
     <div className='flex justify-between items-center bg-cor3  h-12  md:hidden py-8'>
-        <Link to="/" className='text-cor5 opacity-100 px-4 hover:bg-cor2 !font-bold'>HOME</Link>
-        <Link to="/PabuyanNews" className='text-cor5 opacity-100 px-4 hover:bg-cor2 !font-bold'>NEWS</Link>
-        <Link to="/Profile" className='text-cor5 opacity-100 px-4 hover:bg-cor2 !font-bold'>Profile</Link>
+        <Link to="/" className='text-cor5 opacity-100 px-4 hover:bg-cor2 font-bold'>HOME</Link>
+        <Link to="/PabuyanNews" className='text-cor5 opacity-100 px-4 hover:bg-cor2 font-bold'>NEWS</Link>
+        <Link to="/Profile" className='text-cor5 opacity-100 px-4 hover:bg-cor2 font-bold'>Profile</Link>
     </div>
 </div>
   )

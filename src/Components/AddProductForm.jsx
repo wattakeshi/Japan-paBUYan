806c0bd2c3f9d11973f41438c2cb55b4fcba0006
wattakeshi <takeshi.watanabe@live.com>
2025-12-29 15:24:36 +0900

    fix: ProductForm mobile design

--- src/Components/AddProductForm.jsx
+++ src/Components/AddProductForm.jsx
@@ -5,7 +5,7 @@ const AddProductForm = () => {
 <div className="flex flex-col items-center w-full max-w-lg mx-auto gap-6 mt-10">
   
 <div className="w-full flex flex-col sm:flex-row items-center gap-4">
-     <label className="w-full sm:w-32 text-right font-bold text-cor5/80">Product </label>
+     <label className="w-full sm:w-32 text-center sm:text-right font-bold text-cor5/80">Product </label>
      <input 
       type="text"
       placeholder="Product name"
@@ -14,23 +14,22 @@ const AddProductForm = () => {
 </div>
 
 <div className="w-full flex flex-col sm:flex-row items-center gap-4">
-     <label className="w-full sm:w-32 text-right font-bold text-cor5/80">Details </label>
+     <label className="w-full sm:w-32 text-center sm:text-right font-bold text-cor5/80">Details </label>
      <input 
       type="text"
       placeholder="Color / Size / Flavor / Brand"
       className="w-full flex-1 bg-white/10 border border-white/20 rounded-full px-6 py-3 text-white outline-none focus:border-cor5"
     />
 </div>
-
-  {/* Quantidade e URL podem ficar na mesma linha se quiser economizar espaço */}
-<div className="w-full flex flex-col sm:flex-row gap-4">
-    <div className="flex-1 flex items-center gap-4">
-       <label className="w-32 sm:w-32 text-right font-bold text-cor5/80">Qty</label>
+<div className="w-full flex flex-col sm:flex-row ">
+    <div className=" flex items-center">
+       <p className="sm:w-32 mx-auto text-center sm:text-right font-bold text-cor5/80">Qty</p>
+       </div>
         <input 
         type="number"
         className="w-full bg-white/10 border border-white/20 rounded-full px-6 py-3 text-white"
       />
-    </div>
+    
 </div>
 
 <div className="w-full flex flex-col sm:flex-row items-center gap-4 mt-2">
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,21 +8,20 @@ const Header = () => {
   return (
  <>
  <div>
-    <div className="hidden md:block">
-    <div className=' bg-cor2 h-[740px] w-full py-20  '>
-          <h1 className='py-4 w-3/4 mx-auto font-bold text-cor5 text-center text-[45px]'> Welcome {username}!</h1>
-      <p className='py-4 w-4/5 mx-auto text-cor5 text-center text-[16px]'>Build your custom request list below. Once we locate your items and have news for you, a member of the Pabuyan team will reach out directly!</p>
-      <AddProductForm />
-    </div>
-    </div>
-    <div className='block md:hidden lg:hidden'>
+      <div className='block md:hidden lg:hidden'>
     <img src={banner} alt="PaBUYan Banner" />
   
-     <h1 className='pt-8 w-3/4 mx-auto font-bold text-cor3 opacity-95 text-center text-[20px]'> Welcome {username}!</h1>
-      <p className=' w-4/5 mx-auto text-cor3 opacity-95 text-center text-[12px]'>Build your custom request list below. Once we locate your items and have news for you, a member of the Pabuyan team will reach out directly!</p>
 
     </div>
   </div>  
+    <div className="">
+    <div className=' bg-cor2 h-[740px] w-full md:py-20  '>
+          <h1 className='py-4 w-3/4 mx-auto font-bold text-cor5 text-center md:text-[45px] text-[28px]'> Welcome {username}!</h1>
+      <p className='md:py-4 w-4/5 mx-auto text-cor5 text-center md:text-[16px] text-[12px]'>Build your custom request list below. Once we locate your items and have news for you, a member of the Pabuyan team will reach out directly!</p>
+      <AddProductForm />
+    </div>
+    </div>
+
     </>
   )
 }
